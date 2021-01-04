b591ed08dab4863009854421e2b7a5ea4d37b359
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-01-04 07:10:36 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,13 @@
+class Ground{
+    constructor(x,y,width,height){
+      var options ={ isStatic: true}
+      
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+      World.add(myWorld,this.body);
+      this.width = width;
+      this.height = height;
+    }
+    
+
+
+}
