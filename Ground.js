725dbc7f62357651ce299da430c225070e44664b
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-01-04 07:19:46 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -2,12 +2,17 @@ class Ground{
     constructor(x,y,width,height){
       var options ={ isStatic: true}
       
-      this.body =  Bodies.rectangle(x,y,width,height,options);
+      this.body =  Bodies.rectangle(x,y,height,width,options);
       World.add(myWorld,this.body);
       this.width = width;
       this.height = height;
     }
     
+show()
+    {
+        rectMode(CENTER);
+        rect(this.body.position.x, this.body.position.y,this.width, this.height);
+    }
 
 
 }
